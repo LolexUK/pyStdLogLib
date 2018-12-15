7b0cab9d53f36f10e4115ddc4ee601002aa01a7a
monkeyboy2805 <monkeyboy2805@gmail.com>
2018-12-15 12:22:40 +0000

    Fix the overridden print function so that it now uses *objects

--- pyLogStd.py
+++ pyLogStd.py
@@ -4,19 +4,21 @@ old_input = input
 old_print = print
 old_stderr = sys.stderr.write
 def stderr_write(string=""):
-    old_stderr(string)
-    if logging_enabled:
-        logging.error(string)
+	old_stderr(string)
+	if logging_enabled:
+		logging.error(string)
 sys.stderr.write = stderr_write
 def input(string=""):
-    string_in = old_input(string)
-    if logging_enabled:
-        logging.info("STRING IN " + string_in)
-    return string_in
+	string_in = old_input(string)
+	if logging_enabled:
+		logging.info("STRING IN " + string_in)
+	return string_in
 logging.basicConfig(level=logging.DEBUG, filename='./OUT.txt') ### Print does not get overridden for some reason and sys.stdout.write needs redirecting. Perhaps redirect print function to sys.stdout.write func call with "\n" (or whatever sep) chosen. Also need to account for file being supplied :face_in_hands:
-def print(string="", string2="", sep=" ", end="\n", file=sys.stdout, flush=False):
-    # type: (string, string, string, string, file_object, boolean) -> None
-    old_print(string, string2, sep = sep, end=end, file=file, flush=flush)
-    if logging_enabled:
-        logging.info(string)
-        logging.info(string2)
+def print(*objects, sep=" ", end="\n", file=sys.stdout, flush=False):
+	# type: (object, string, string, file_object, boolean) -> None
+	old_print(*objects, sep = sep, end=end, file=file, flush=flush)
+	if logging_enabled:
+		for i in range(0, len(objects) - 1): ## Necessary else the logging module will not convert the tuple into the relevant strings
+			if i == len(objects):
+				break
+			logging.info(objects[i])
