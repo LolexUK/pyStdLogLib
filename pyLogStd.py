4ee9c4bb9098f608e2bcf4ae93702ace67d8f3c3
monkeyboy2805 <monkeyboy2805@gmail.com>
2018-12-15 12:13:17 +0000

    Simplify some statements

--- pyLogStd.py
+++ pyLogStd.py
@@ -5,18 +5,18 @@ old_print = print
 old_stderr = sys.stderr.write
 def stderr_write(string=""):
     old_stderr(string)
-    if logging_enabled == True:
+    if logging_enabled:
         logging.error(string)
 sys.stderr.write = stderr_write
 def input(string=""):
     string_in = old_input(string)
-    if logging_enabled == True:
+    if logging_enabled:
         logging.info("STRING IN " + string_in)
     return string_in
 logging.basicConfig(level=logging.DEBUG, filename='./OUT.txt') ### Print does not get overridden for some reason and sys.stdout.write needs redirecting. Perhaps redirect print function to sys.stdout.write func call with "\n" (or whatever sep) chosen. Also need to account for file being supplied :face_in_hands:
 def print(string="", string2="", sep=" ", end="\n", file=sys.stdout, flush=False):
     # type: (string, string, string, string, file_object, boolean) -> None
     old_print(string, string2, sep = sep, end=end, file=file, flush=flush)
-    if logging_enabled == True:
+    if logging_enabled:
         logging.info(string)
         logging.info(string2)
